7654fb51d5daa7d3ba7b589b6956203229082a9a
Sergey Andreev <aandreev.sm@gmail.com>
2023-05-23 23:07:00 +0300

    Добавлен список дел на React

--- react-todo-app/src/App.js
+++ react-todo-app/src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useState } from 'react';
 
-function App() {
+let id = 0;
+
+const DEFAULT_TASK = { id: 0, title: '', description: '', isDone: false };
+
+const App = () => {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState(DEFAULT_TASK);
+
+  const onSubmit = useCallback((e) => {
+    const task = {
+      ...newTask,
+      id: ++id,
+    };
+
+    setTasks([task, ...tasks]);
+
+    setNewTask(DEFAULT_TASK)
+
+    e.preventDefault();
+  }, [tasks, newTask]);
+
+  const onDeleteTask = useCallback((id) => {
+    const newTasks = tasks.filter(task => task.id !== id);
+    setTasks(newTasks);
+  }, [tasks]);
+
+  const onUpdateTask = useCallback((task) => {
+    const newTasks = tasks.map(_task => _task.id === task.id ? { ...task } : _task)
+    setTasks(newTasks);
+  }, [tasks]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <div>
+        <form onSubmit={onSubmit}>
+          <input type="text" value={newTask.title} onInput={(e) => setNewTask({
+              ...newTask,
+              title: e.target.value,
+            })}
+          />
+          <input type="text" value={newTask.description} onInput={(e) => setNewTask({
+              ...newTask,
+              description: e.target.value,
+            })}
+          />
+
+          <button type="submit">Добавить</button>
+        </form>
+      </div>
+
+      <div>
+        <ul>
+          {tasks.map(task => (
+            <li key={task.id}>
+              <div>{task.title}</div>
+              <div>{task.description}</div>
+              <input type="checkbox" name="isDone" value={task.isDone} onChange={() => onUpdateTask({ ...task, isDone: !task.isDone })} />
+              <button onClick={() => onDeleteTask(task.id)}>delete</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+
     </div>
   );
 }
